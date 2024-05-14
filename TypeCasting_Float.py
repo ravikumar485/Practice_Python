50b32b4c0336b352a14e59affc8d1d9d1d5f3c64
ravi <uare@protonmail.com>
2024-05-14 16:49:20 +0530

    hhhhh

---
+++ TypeCasting_Float.py
@@ -0,0 +1,29 @@
+print("Hello World")
+s= float(10)
+print(s)
+
+s=10.2
+
+a=float(s)
+print(a)
+
+s="10.2"
+a=float(s)
+print(a)
+
+s=True
+a=float(s)
+print(a)
+
+# s='OB123'
+# a=float(s)
+# print(a)
+
+# s='ss'
+# a=float(s)
+# print(s)
+
+
+s=float(0x123)
+a=float(s)
+print(a)
