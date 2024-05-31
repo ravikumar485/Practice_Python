2a390b901dddc37a18fa1cfdbf79413cdbe2fd28
ravi <uare@protonmail.com>
2024-05-31 10:00:11 +0530

    latest

---
+++ functions_notes/check_module.py
@@ -0,0 +1,10 @@
+'''hello im nee module'''
+import psd
+a=10
+b=12
+
+def fn_calc(x,y):
+    return x + y
+print(__doc__)
+
+print(__name__)
---
+++ functions_notes/fnames.py
@@ -0,0 +1,19 @@
+'''hhhhhhhhhhhhhhhhhh'''
+
+# import os
+import check_module
+
+# print("filename =",__file__)
+
+# abpath=os.path.abspath(__file__)
+# print("abpath =", abpath)
+
+
+# dirname=os.path.dirname(abpath)
+# print("dirname =", dirname)
+
+
+
+print(__doc__)
+
+print(__name__)
